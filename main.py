8718e5d89aade7cfb711d92b1cf7791deb1a72f5
vlcastillo <vlcastillo@uc.cl>
2017-11-21 10:50:42 -0300

    html

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Proyecto Capstone Groupon!"
+    return render_template("pagina.html")
 
-@app.route("/otro")
-def otros():
-    with open('test.txt', 'r') as archivo:
-        for linea in archivo:
-            string= linea
-            
-    return string
+@app.route("/demanda")
+def demanda():
+    return "Predicción demanda"
+
+@app.route("/desempeno")
+def desempeno():
+    return "Predicción desempeño"
+
 
 if __name__ == "__main__":
     app.run()
