9accfabb1246d854c2f743a2637535d72f8085f4
vlcastillo <vlcastillo@uc.cl>
2017-11-23 13:05:47 -0300

    eficiencia

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ import scipy
 app = Flask(__name__)
 from multiprocessing import Pool
 
+def estadistica_demanda():
+    cash_in = {}
+    with open("GR_categoria.csv", "r") as datos:
+        for columna in datos:
+            columna = columna.strip().split(',')
+            columna = [x.replace('"', '') for x in columna]
+            if columna[0] == "CL":
+                if len(columna) == 8:
+                    if columna[3] in cash_in.keys():
+                        cash_in[columna[3]][dt.date(*[int(x) for x in columna[1].split("-")])] = int(columna[5])
+                    else:
+                        cash_in[columna[3]] = {dt.date(*[int(x) for x in columna[1].split("-")]): int(columna[5])}
+    categorias = [Categoria(x, cash_in[x]) for x in cash_in.keys()]
+
+    string = ""
+    for categoria in categorias:
+        string += categoria.nombre + ': ' + str(categoria.pvalue) + '\n'
+    return string
+
+pvalues = estadistica_demanda()
+
 class Categoria:
     def __init__(self, nombre, cash_in):
         self.nombre = nombre
@@ -39,6 +60,33 @@ class Categoria:
                 post.append(residuos[i])
         self.pvalue = scipy.stats.ttest_ind(pre, post, equal_var = False).pvalue
 
+@app.route("/")
+def hello():
+    return render_template("pagina.html")
+
+@app.route("/demanda")
+def demanda():
+    """response = requests.post("https://app.dominodatalab.com/v1/vlcastillo/groupon/endpoint",
+    headers = {
+        "X-Domino-Api-Key": "1WzC8BAZ2sAAJRFlRPg1joyGqakETAb8G8fL9VUp3kBIILtTC1yJzFBXa15bPl72",
+        "Content-Type": "application/json"
+    },
+    json = {
+        "parameters": [2.5]
+    })"""
+    global pvalues
+    return pvalues
+
+@app.route("/desempeno")
+def desempeno():
+    return "Predicción desempeño"
+
+
+if __name__ == "__main__":
+    app.run()
+    
+    
+"""
 def pvalue(cash_in):
     actual = min(cash_in.keys())
     final = max(cash_in.keys())
@@ -66,51 +114,10 @@ def pvalue(cash_in):
         else:
             post.append(residuos[i])
     return scipy.stats.ttest_ind(pre, post, equal_var = False).pvalue
-            
-@app.route("/")
-def hello():
-    return render_template("pagina.html")
-
-@app.route("/demanda")
-def demanda():
-    """response = requests.post("https://app.dominodatalab.com/v1/vlcastillo/groupon/endpoint",
-    headers = {
-        "X-Domino-Api-Key": "1WzC8BAZ2sAAJRFlRPg1joyGqakETAb8G8fL9VUp3kBIILtTC1yJzFBXa15bPl72",
-        "Content-Type": "application/json"
-    },
-    json = {
-        "parameters": [2.5]
-    })"""
     
-    return estadistica_demanda()
-
-@app.route("/desempeno")
-def desempeno():
-    return "Predicción desempeño"
-
-def estadistica_demanda():
-    cash_in = {}
-    with open("GR_categoria.csv", "r") as datos:
-        for columna in datos:
-            columna = columna.strip().split(',')
-            columna = [x.replace('"', '') for x in columna]
-            if columna[0] == "CL":
-                if len(columna) == 8:
-                    if columna[3] in cash_in.keys():
-                        cash_in[columna[3]][dt.date(*[int(x) for x in columna[1].split("-")])] = int(columna[5])
-                    else:
-                        cash_in[columna[3]] = {dt.date(*[int(x) for x in columna[1].split("-")]): int(columna[5])}
-    #categorias = [Categoria(x, cash_in[x]) for x in cash_in.keys()]
-    pool = Pool(4)
+        pool = Pool(4)
     pvalues = pool.map(pvalue, [cash_in[x] for x in cash_in.keys()])
     pool.close()
     pool.join()
     string = ""
-    for p in pvalues:
-        string += str(p) +'\n'
-    return string
-
-
-if __name__ == "__main__":
-    app.run()
-    
+            """
